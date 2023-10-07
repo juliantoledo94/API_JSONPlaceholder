d6993517f6c10d8f6d417f6a013cd2424f2ce96e
Julian Toledo <84879981+juliantoledo94@users.noreply.github.com>
2023-10-07 12:55:28 -0300

    feat: create base html and js files

---
+++ main.js
@@ -0,0 +1,7 @@
+/*
+Crear una página index.html para mostrar una lista de usuarios.
+Utilizar la API JSONPlaceholder (https://jsonplaceholder.typicode.com/users) para obtener una lista de usuarios.
+Mostrar el nombre y el correo electrónico de cada usuario en la página principal.
+Cuando se hace clic en un usuario, abrir un modal con más detalles del usuario, como la dirección y el número de teléfono.
+ 
+*/
