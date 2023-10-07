13e5519442702a2399dc062e259258fcc7cb3e33
Julian Toledo <84879981+juliantoledo94@users.noreply.github.com>
2023-10-07 16:45:38 -0300

    feat: add getUsuariosFromApi

--- main.js
+++ main.js
@@ -5,3 +5,22 @@ Mostrar el nombre y el correo electrónico de cada usuario en la página princip
 Cuando se hace clic en un usuario, abrir un modal con más detalles del usuario, como la dirección y el número de teléfono.
  
 */
+
+const getUsuariosFromApi = () =>{
+    return fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then(response => response.json())
+    .then( data => data.map((usuarios)=> {
+        const {name, email} = usuarios;
+        console.log(name, email)
+        return {name, email}
+    }))
+    /*
+    .then(data => {
+        const nombres = data.map(usuario => usuario.name);
+        console.log(nombres); // Imprime un array con los nombres
+        return nombres; // Si deseas devolver los nombres para usarlos en otro lugar
+    });
+    * */
+}
+
+getUsuariosFromApi();
